063786a66b0897259c8de10ae049026c6228723c
shyamz-22 <shykart2203@gmail.com>
2017-12-17 13:37:16 +0100

    after every successful build publish a snapshot release try four :rocket:

--- publish/publish-patch.sh
+++ publish/publish-patch.sh
@@ -5,29 +5,34 @@ set -e
 
 
 main() {
-  git checkout master
 
-  local CURRENT_BRANCH=$(git branch -a | grep 'release/' | tail -n 1)
+  CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
   echo "Current Branch Version : ${CURRENT_BRANCH}"
+
+  echo "Current Branch Version with travis : ${TRAVIS_BRANCH}"
+
+  if [ ${TRAVIS_BRANCH} != "master" ]; then
+      git checkout ${CURRENT_BRANCH}
+
+      ./gradlew clean build  -PSONATYPE_USERNAME=${SONATYPE_USERNAME} -PSONATYPE_PASSWORD=${SONATYPE_PASSWORD}
+
+      ./gradlew versionFile  -PSONATYPE_USERNAME=${SONATYPE_USERNAME} -PSONATYPE_PASSWORD=${SONATYPE_PASSWORD}
+
+      export $(cat build/version.properties | xargs)
+
+      GIT_TAG=${VERSION_DISPLAY}
+
+      echo "Tag Version is : ${GIT_TAG}"
+
+      ./gradlew uploadArchives  -PSONATYPE_USERNAME=${SONATYPE_USERNAME} -PSONATYPE_PASSWORD=${SONATYPE_PASSWORD}
+
+      git tag ${GIT_TAG}
+
+      git push --tag
+
+  fi
   
-  git checkout ${CURRENT_BRANCH}
-
-  ./gradlew clean build  -PSONATYPE_USERNAME=${SONATYPE_USERNAME} -PSONATYPE_PASSWORD=${SONATYPE_PASSWORD}
-
-  ./gradlew versionFile  -PSONATYPE_USERNAME=${SONATYPE_USERNAME} -PSONATYPE_PASSWORD=${SONATYPE_PASSWORD}
-
-  export $(cat build/version.properties | xargs)
-
-  GIT_TAG=${VERSION_DISPLAY}
-
-  echo "Tag Version is : ${GIT_TAG}"
-
-  ./gradlew uploadArchives  -PSONATYPE_USERNAME=${SONATYPE_USERNAME} -PSONATYPE_PASSWORD=${SONATYPE_PASSWORD}
-
-  git tag ${GIT_TAG}
-
-  git push --tag
 
 }
 
--- publish/publish-release.sh
+++ publish/publish-release.sh
@@ -5,27 +5,33 @@ set -e
 
 
 main() {
-  git checkout master
 
-  local CURRENT_BRANCH=$(git branch -a | grep 'release/' | tail -n 1)
+
+  CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
   echo "Current Branch Version : ${CURRENT_BRANCH}"
+
+  echo "Current Branch Version with travis : ${TRAVIS_BRANCH}"
+
+  if [ ${TRAVIS_BRANCH} != "master" ]; then
   
-  git checkout ${CURRENT_BRANCH}
+          git checkout ${CURRENT_BRANCH}
 
-  ./gradlew clean build -PVERSIONING_RELEASE_BUILD=true -PVERSIONING_RELEASE_MODE=tag -PVERSIONING_DISPLAY_MODE=base  -PSONATYPE_USERNAME=${SONATYPE_USERNAME} -PSONATYPE_PASSWORD=${SONATYPE_PASSWORD}
+          ./gradlew clean build -PVERSIONING_RELEASE_BUILD=true -PVERSIONING_RELEASE_MODE=tag -PVERSIONING_DISPLAY_MODE=base  -PSONATYPE_USERNAME=${SONATYPE_USERNAME} -PSONATYPE_PASSWORD=${SONATYPE_PASSWORD}
 
-  ./gradlew versionFile -PVERSIONING_RELEASE_BUILD=true -PVERSIONING_RELEASE_MODE=tag -PVERSIONING_DISPLAY_MODE=base  -PSONATYPE_USERNAME=${SONATYPE_USERNAME} -PSONATYPE_PASSWORD=${SONATYPE_PASSWORD}
+          ./gradlew versionFile -PVERSIONING_RELEASE_BUILD=true -PVERSIONING_RELEASE_MODE=tag -PVERSIONING_DISPLAY_MODE=base  -PSONATYPE_USERNAME=${SONATYPE_USERNAME} -PSONATYPE_PASSWORD=${SONATYPE_PASSWORD}
 
-  export $(cat build/version.properties | xargs)
+          export $(cat build/version.properties | xargs)
 
-  GIT_TAG="${VERSION_DISPLAY}"
+          GIT_TAG="${VERSION_DISPLAY}"
 
-  ./gradlew uploadArchives -PVERSIONING_RELEASE_BUILD=true -PVERSIONING_RELEASE_MODE=tag -PVERSIONING_DISPLAY_MODE=base  -PSONATYPE_USERNAME=${SONATYPE_USERNAME} -PSONATYPE_PASSWORD=${SONATYPE_PASSWORD}
+          ./gradlew uploadArchives -PVERSIONING_RELEASE_BUILD=true -PVERSIONING_RELEASE_MODE=tag -PVERSIONING_DISPLAY_MODE=base  -PSONATYPE_USERNAME=${SONATYPE_USERNAME} -PSONATYPE_PASSWORD=${SONATYPE_PASSWORD}
 
-  git tag ${GIT_TAG}
+          git tag ${GIT_TAG}
 
-  git push --tag
+          git push --tag
+
+  fi
 
 }
 
