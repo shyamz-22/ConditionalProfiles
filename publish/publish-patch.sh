a2eedab9e310f4ccd951534a6a6c4a535624317b
shyamz-22 <shykart2203@gmail.com>
2017-12-20 18:39:32 +0100

    just print out env variables in travis

--- publish/publish-patch.sh
+++ publish/publish-patch.sh
@@ -7,26 +7,28 @@ set -e
 main() {
 
   echo "Current Branch Version with travis : ${TRAVIS_BRANCH}"
+   echo "Current TAG Version with travis : ${TRAVIS_TAG}"
+./gradlew versionFile  -PSONATYPE_USERNAME=${SONATYPE_USERNAME} -PSONATYPE_PASSWORD=${SONATYPE_PASSWORD}
 
-  if [ ${TRAVIS_BRANCH} != "master" ]; then
+#  if [ ${TRAVIS_BRANCH} != "master" ]; then
 
-      ./gradlew clean build  -PSONATYPE_USERNAME=${SONATYPE_USERNAME} -PSONATYPE_PASSWORD=${SONATYPE_PASSWORD}
+#      ./gradlew clean build  -PSONATYPE_USERNAME=${SONATYPE_USERNAME} -PSONATYPE_PASSWORD=${SONATYPE_PASSWORD}
+#
+#      ./gradlew versionFile  -PSONATYPE_USERNAME=${SONATYPE_USERNAME} -PSONATYPE_PASSWORD=${SONATYPE_PASSWORD}
+#
+#      export $(cat build/version.properties | xargs)
+#
+#      GIT_TAG=${VERSION_DISPLAY}
+#
+#      echo "Tag Version is : ${GIT_TAG}"
+#
+#      ./gradlew uploadArchives  -PSONATYPE_USERNAME=${SONATYPE_USERNAME} -PSONATYPE_PASSWORD=${SONATYPE_PASSWORD}
+#
+#      git tag ${GIT_TAG}
+#
+#      git push --tag
 
-      ./gradlew versionFile  -PSONATYPE_USERNAME=${SONATYPE_USERNAME} -PSONATYPE_PASSWORD=${SONATYPE_PASSWORD}
-
-      export $(cat build/version.properties | xargs)
-
-      GIT_TAG=${VERSION_DISPLAY}
-
-      echo "Tag Version is : ${GIT_TAG}"
-
-      ./gradlew uploadArchives  -PSONATYPE_USERNAME=${SONATYPE_USERNAME} -PSONATYPE_PASSWORD=${SONATYPE_PASSWORD}
-
-      git tag ${GIT_TAG}
-
-      git push --tag
-
-  fi
+#  fi
   
 
 }
